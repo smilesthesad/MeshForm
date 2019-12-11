1171a5caf0d008286a310c68feba6ed8f24bb873
Marcus Chong <marcus.jn.chong@gmail.com>
2019-12-11 11:58:00 -0500

    some sample view data stuff ig

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import { Card, Slider, Input, Text } from 'react-native-elements'
 import { ExpoLinksView } from '@expo/samples';
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+export default class LinksScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            teams: [],
+        }
+    }
+
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <Card>
+                    <Text> hi </Text>
+                </Card>
+            </ScrollView>
+          );
+    }
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Links',
+  title: 'Data View',
 };
 
 const styles = StyleSheet.create({
